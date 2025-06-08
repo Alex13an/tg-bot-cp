f3c4aee331245eb210a080e441d4690dd11f7f76
Alexandr An <a76077817@gmail.com>
2025-06-08 23:19:34 +0500

    feat: change payment type

--- bot.ts
+++ bot.ts
@@ -19,6 +19,7 @@ interface SessionData {
   fio: string;
   phone: string;
   type: number | null;
+  re: boolean;
 }
 
 type MyContext = Context & SessionFlavor<SessionData>;
@@ -30,6 +31,7 @@ function initial(): SessionData {
     fio: "",
     phone: "",
     type: null,
+    re: false,
   };
 }
 bot.use(session({ initial }));
@@ -48,15 +50,54 @@ bot.command("start", async (ctx) => {
   });
 });
 
-bot.hears([...subscriptionVariants].map(s => s.description), async (ctx) => {
+bot.hears(
+  [...subscriptionVariants].map((s) => s.description),
+  async (ctx) => {
     const text = ctx.message?.text?.trim();
-    const type = subscriptionVariants.find(v => v.description === text)
-    ctx.session.type = type?.id || 1
+    const type = subscriptionVariants.find((v) => v.description === text);
+    ctx.session.type = type?.id || 1;
 
-    ctx.session.step = "awaiting_fio";
-    await ctx.reply("Введите ваше ФИО:", {
-      reply_markup: { remove_keyboard: true },
-    });
+    if (!ctx.session.re) {
+      ctx.session.step = "awaiting_fio";
+      await ctx.reply("Введите ваше ФИО:", {
+        reply_markup: { remove_keyboard: true },
+      });
+    } else {
+      const existing = db
+        .prepare(`SELECT id FROM users WHERE chat_id = ?`)
+        .get(ctx.chat.id);
+      const userId = existing?.id || uuidv1();
+
+      const insert = db.prepare(`
+      INSERT INTO users (id, chat_id, fio, phone, sub_type)
+      VALUES (?, ?, ?, ?, ?)
+      ON CONFLICT(chat_id) DO UPDATE SET fio=excluded.fio, phone=excluded.phone, sub_type=excluded.sub_type
+    `);
+      insert.run(
+        userId,
+        ctx.chat.id,
+        ctx.session.fio,
+        ctx.session.phone,
+        ctx.session.type
+      );
+
+      const url = `${process.env.BASE_URL}?token=${userId}`;
+      const inlineKeyboard = new InlineKeyboard()
+        .url("Оплатить", url || "Не удалось получить ссылку...")
+        .row()
+        .text("Выбрать другой тариф", "select_another");
+
+      const curent_variant =
+        subscriptionVariants.find((v) => v.id === ctx.session.type) ||
+        subscriptionVariants[0];
+      const pay_message = get_pay_message(
+        curent_variant.title,
+        curent_variant.price
+      );
+      await ctx.reply(pay_message, {
+        reply_markup: inlineKeyboard,
+      });
+    }
   }
 );
 
@@ -97,20 +138,42 @@ bot.on("message:text", async (ctx) => {
     );
 
     const url = `${process.env.BASE_URL}?token=${userId}`;
-    const inlineKeyboard = new InlineKeyboard().url(
-      "Оплатить",
-      url || "Не удалось получить ссылку..."
-    );
+    const inlineKeyboard = new InlineKeyboard()
+      .url("Оплатить", url || "Не удалось получить ссылку...")
+      .row()
+      .text("Выбрать другой тариф", "select_another");
 
-    const curent_variant = subscriptionVariants.find(v => v.id === ctx.session.type) || subscriptionVariants[0]
-    const pay_message = get_pay_message(curent_variant.title, curent_variant.price)
+    const curent_variant =
+      subscriptionVariants.find((v) => v.id === ctx.session.type) ||
+      subscriptionVariants[0];
+    const pay_message = get_pay_message(
+      curent_variant.title,
+      curent_variant.price
+    );
     await ctx.reply(pay_message, {
       reply_markup: inlineKeyboard,
     });
 
     // Очистка сессии
-    ctx.session = { step: null, fio: "", phone: "", type: null };
+    // ctx.session = { step: null, fio: "", phone: "", type: null };
   }
+});
+
+bot.callbackQuery("select_another", async (ctx) => {
+  await ctx.answerCallbackQuery(); // убирает "часики"
+  ctx.session.step = null;
+  ctx.session.re = true;
+  await ctx.reply(greet_message, {
+    reply_markup: new Keyboard()
+      .oneTime()
+      .text(subscriptionVariants[0].description)
+      .text(subscriptionVariants[1].description)
+      .text(subscriptionVariants[2].description)
+      .text(subscriptionVariants[3].description)
+      .row()
+      .toFlowed(1)
+      .oneTime(),
+  });
 });
 
 export default bot;
